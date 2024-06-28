5d8acd6d2ae380cc0a9e2db3c353e4dcac052eef
Bright Gee Varghese <brightlsom@gmail.com>
2024-06-28 11:31:16 -0500

    removed unused imports in MainActivity

--- app/src/main/java/com/example/boxlayout/MainActivity.kt
+++ app/src/main/java/com/example/boxlayout/MainActivity.kt
@@ -4,24 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Alignment
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.platform.LocalConfiguration
-
 import com.example.boxlayout.ui.theme.BoxLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -84,23 +78,6 @@ fun AlignmentInBox(modifier: Modifier = Modifier) {
         Text(
             text = "Bottom End",
             modifier.align(Alignment.BottomEnd)
-        )
-    }
-}
-
-@Composable
-fun TextCell(text: String, modifier: Modifier = Modifier,  fontSize: Int = 150 ) {
-
-    val cellModifier = Modifier
-        .padding(4.dp)
-        .border(width = 5.dp, color = Color.Black)
-
-    Surface {
-        Text(
-            text = text, cellModifier.then(modifier),
-            fontSize = fontSize.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
         )
     }
 }
